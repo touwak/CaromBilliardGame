b6f1f6878e99e33c5fb0044260d1253ab6943b9f
touwak <claramuntgi@esat-alumni.com>
2019-01-17 14:41:33 +0100

    Increment force in time

--- Assets/Scripts/PlayerBehaviour.cs
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -9,23 +9,54 @@ public class PlayerBehaviour : MonoBehaviour {
     Camera camera;
     Rigidbody rigidbody;
 
-    float strength = 100.0f;
+    [SerializeField]
+    float minStrength = 10.0f;
+    [SerializeField]
+    float maxStrength = 2000.0f;
+    [SerializeField]
+    float accelerationTime = 3.0f;
+    float acceleration;
+    float finalStrength;
     #endregion
 
 	void Start () {
         camera = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
         rigidbody = GetComponent<Rigidbody>();
+
+        acceleration = (maxStrength - minStrength) / accelerationTime;
+        finalStrength = minStrength;
 	}
 
     void Update() {
-        if (Input.GetKeyDown(KeyCode.Space)) {
+
+        if (Input.GetButton("HitBall")) {
+            IncrementHitForce();
+        }
+        if (Input.GetButtonUp("HitBall")) {
             HitBall();
         }
     }
 
+    /// <summary>
+    /// Hit the ball in the forward direction of the camera
+    /// </summary>
     void HitBall() {
         Vector3 direction = camera.transform.forward;
         direction.y = 0.0f;
-        rigidbody.AddForce(direction * strength);
+        rigidbody.AddForce(direction * finalStrength);
+
+        // reset strength
+        finalStrength = minStrength;
+    }
+
+    /// <summary>
+    /// Increment strength in time
+    /// </summary>
+    void IncrementHitForce() {
+        if(finalStrength < maxStrength) {
+            finalStrength += acceleration * Time.deltaTime;
+        }
+
+        Debug.Log("acceleration: " + finalStrength);
     }
 }
